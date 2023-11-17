004e01a5c3d63164d0bf03d0c1decbfdc2fb2b6b
Jaider Rendon <jaiderrendon66@gmail.com>
2023-11-16 22:13:56 -0500

    siuu

--- Python/Gestion Pedidos/figura.py
+++ Python/Gestion Pedidos/figura.py
@@ -10,7 +10,7 @@ class Figura:
         return f"Nombre : {self.nombre_figura}, Tamaño : {self.tamaño} cm, Precio : ${self.precio}, Cantidad de yeso : {self.cantidad_yeso} lb, Unidades Pedidas: {self.cantidad_unidades}"
     
     def obtener_precio(self):
-        return self.precio
+        return str(int(self.precio)*int(self.cantidad_unidades))
     
     def modificar_figura(self, nombre_figura, tamaño, precio, cantidad_yeso, cantidad_unidades):
         self.nombre_figura = nombre_figura
