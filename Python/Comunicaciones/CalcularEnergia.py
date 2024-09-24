f8412b85f1472c03f0426d16ed8db9530068a7e9
Jaider Rendón <70232955+VOIDX66@users.noreply.github.com>
2024-09-24 18:03:34 -0500

    Delete Python/Comunicaciones directory

--- Python/Comunicaciones/CalcularEnergia.py
+++
@@ -1,6 +0,0 @@
-import sympy as sym
-
-t = sym.Symbol('t',real=True)
-energia = sym.integrate((sym.exp(-t))**2,(t,0, sym.oo))
-
-sym.pprint(energia)#oo
--- Python/Comunicaciones/Integral.py
+++
@@ -1,7 +0,0 @@
-import sympy as sym
-
-t = sym.Symbol('t',real=True)
-i1 = sym.integrate(1*sym.sin(t),(t,0,sym.pi))+sym.integrate(-1*sym.sin(t),(t,sym.pi,2*sym.pi))
-i2 = sym.integrate(sym.sin(t)**2,(t,0,2*sym.pi))
-solucion = i1/i2
-sym.pprint(solucion)#oo
